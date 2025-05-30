602ec8853355b6069a7ccdbcbcd13cc9c3d9d765
Balaphani <phnkumar51@gmail.com>
2025-05-30 09:13:51 +0100

    functions

---
+++ function.sh
@@ -0,0 +1,10 @@
+# Function: Assigning a name to a set of commands is called functions.
+#        Syntax: function_name(){
+#                               commands
+#                                }
+# How to Access the function - Syntax: Function_name
+Devops(){
+  echo Learning Shell Scripts
+}
+
+Devops
