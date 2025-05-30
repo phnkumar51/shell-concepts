b8fa9137bb8b1527e3e6776ed1bd08bbcf2472bc
Balaphani <phnkumar51@gmail.com>
2025-05-30 08:56:19 +0100

    commit

--- var.sh
+++ var.sh
@@ -2,11 +2,13 @@
 
 # 1. Variable: Assigning a name to a set of data is called Variable.
 #           Syntax: var = data
-#                Example: a=10; b=15; 
+#                Example: a=10; b=15;
 #    Command "echo": This command will help in printing the value of a variable
 # 2. Syntax to access a variable - $var
-#
+
 
 a=10;
 
 echo $a
+
+# execute this commands
